9b58dfc190db4938c8c9cbfd09e9a60d6a092197
Andres Balbuena <77251658+AndresBLV@users.noreply.github.com>
2022-03-22 17:54:48 +0000

    Inicio proyecto

--- book.py
+++ book.py
@@ -6,3 +6,32 @@ class Book():
     self.__disponible = disponible
     self.__prestamo = 0
 
+  def get_cota(self):
+    return self.__cota
+
+  def get_titulo(self):
+    return self.__titulo
+
+  def get_serial(self):
+    return self.__serial
+
+  def get_disponible(self):
+    return self.__disponible
+
+  def get_prestamo(self):
+    return self.__prestamo
+
+  def set_cota(self,new_cota):
+    self.__cota = new_cota
+
+  def set_titulo(self,new_titulo):
+    self.__titulo = new_titulo
+
+  def set_serial(self,new_serial):
+    self.__serial = new_serial
+
+  def set_disponible(self,new_disponible):
+    self.__disponible = new_disponible
+
+  def set_prestamo(self,new_prestamo):
+    self.__prestamo = new_prestamo
